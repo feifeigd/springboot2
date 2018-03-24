317fc2f2a056e540388fd014027b7db95d113232
luofei <502207456@qq.com>
2018-03-25 01:00:10 +0800

    添加程序入口

---
+++ src/main/java/com/d7kj/Application.java
@@ -0,0 +1,13 @@
+package com.d7kj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	/// 抄袭入口点
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
