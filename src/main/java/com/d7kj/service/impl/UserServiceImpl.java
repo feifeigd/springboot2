67c5ae1b0a9eb71de15301abed91feb389208661
luofei <502207456@qq.com>
2018-04-05 16:15:27 +0800

    UserServiceImpl.java

---
+++ src/main/java/com/d7kj/service/impl/UserServiceImpl.java
@@ -0,0 +1,40 @@
+package com.d7kj.service.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.d7kj.entity.User;
+import com.d7kj.service.UserService;
+
+public class UserServiceImpl implements UserService {
+
+	@Override
+	public List<User> allUser() {
+		return sampleUser(5);
+	}
+
+	@Override
+	public User getUserById(int id) {
+		User user = sampleUser(1).get(0);
+		user.setId(id);
+		return user;
+	}
+	
+	private List<User> sampleUser(int num) {
+		List<User> list = new ArrayList<User>(num);
+		for(int i = 0; i < num; ++i) {
+			User user = new User();
+			user.setId(i);
+			user.setName("name" + i);
+			list.add(user);
+		}
+		return null;
+	}
+
+	@Override
+	public void updateUser(int id, Integer type) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
