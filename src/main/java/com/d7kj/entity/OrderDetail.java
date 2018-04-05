3df879272778b4d06f9b1ac36b6ff59d29009eda
luofei <502207456@qq.com>
2018-04-05 16:27:09 +0800

    add entity

---
+++ src/main/java/com/d7kj/entity/OrderDetail.java
@@ -0,0 +1,9 @@
+package com.d7kj.entity;
+
+import lombok.Data;
+
+@Data
+public class OrderDetail {
+	Long 	id;
+	String 	name;
+}
---
+++ src/main/java/com/d7kj/entity/User.java
@@ -0,0 +1,9 @@
+package com.d7kj.entity;
+
+import lombok.Data;
+
+@Data
+public class User {
+	private int id;
+	String name;
+}
