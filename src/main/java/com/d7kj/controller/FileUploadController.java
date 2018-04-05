9bff30df828c418367e62bac3ca7a412247c090d
luofei <502207456@qq.com>
2018-04-06 00:15:41 +0800

    文件上传

---
+++ src/main/java/com/d7kj/controller/FileUploadController.java
@@ -0,0 +1,27 @@
+package com.d7kj.controller;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+@RequestMapping("/upload")
+public class FileUploadController {
+
+	@PostMapping("/form")
+	@ResponseBody
+	public String handleFromUpload(MultipartFile file) throws IOException {
+		if(file.isEmpty())
+			return "failure";
+		String fileName = file.getOriginalFilename();
+		File dest = new File(fileName);
+		file.transferTo(dest);
+		return "success";
+	}
+}
