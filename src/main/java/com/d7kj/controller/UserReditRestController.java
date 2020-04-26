821f9a378d6bdddaaea7a317646aaa9099e40927
feifeigd <feifeigd@21cn.com>
2020-04-26 11:54:19 +0800

    rest controller

---
+++ src/main/java/com/d7kj/controller/UserReditRestController.java
@@ -0,0 +1,15 @@
+package com.d7kj.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/// 注解RestController相当于注解Controller和ResponseBody的合体。
+@RestController
+public class UserReditRestController {
+
+    @RequestMapping(value = "/usercredit/{id}")
+    public Integer getCreditLevel(@PathVariable String id){
+        return Integer.parseInt(id);
+    }
+}
