ffb50cac8c1a318afa5d15fe98ed19a5a38d2f19
feifeigd <feifeigd@21cn.com>
2020-05-31 03:07:19 +0800

    升级版本

--- src/main/java/com/d7kj/conf/JacksonConf.java
+++ src/main/java/com/d7kj/conf/JacksonConf.java
@@ -8,9 +8,11 @@ import java.text.SimpleDateFormat;
 
 @Configuration
 public class JacksonConf {
+    // 开机会执行该函数
     @Bean
     public ObjectMapper getObjectMapper(){
         ObjectMapper objectMapper = new ObjectMapper();
+        // 默认格式是 "2020-05-30T02:28:33.967+0000"
         objectMapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
         return objectMapper;
     }
