aacd50472bb2ba52ff91d13282d0a291798a5ffb
feifeigd <feifeigd@21cn.com>
2020-06-02 00:16:49 +0800

    redis 字符串

--- src/main/java/com/d7kj/controller/RedisStringController.java
+++ src/main/java/com/d7kj/controller/RedisStringController.java
@@ -1,5 +1,8 @@
 package com.d7kj.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,8 +11,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/strredis")
 public class RedisStringController {
 
+    @Autowired
+    StringRedisTemplate redisClient;
+
     @RequestMapping("/setget.html")
     public @ResponseBody String env(String para)throws Exception{
-        return "";
+        ValueOperations<String, String> redis = redisClient.opsForValue();
+        redis.set("testenv", para);
+        return redis.get("testenv");
     }
 }
